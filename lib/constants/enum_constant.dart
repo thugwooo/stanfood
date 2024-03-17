b844f57619184ed50ecc13ab1e84f507e9e893e5
LeeSeokWoo <indians1720@gmail.com>
2024-03-17 16:31:21 +0900

    broadcast model 추가

--- lib/constants/enum_constant.dart
+++ lib/constants/enum_constant.dart
@@ -2,7 +2,7 @@ enum BottomNaviType { Home, Map, Register }
 
 enum RegisterTabType { Main, Stan, Restaurant, BroadCast, Mukbang }
 
-enum JobType { Youtuber, Entertainer, Blogger, Influencer }
+enum JobType { Youtuber, Entertainer, Influencer }
 
 extension JobTypeExtension on JobType {
   String description() {
@@ -11,12 +11,23 @@ extension JobTypeExtension on JobType {
         return '유튜버';
       case JobType.Entertainer:
         return '연예인';
-      case JobType.Blogger:
-        return '블로거';
       case JobType.Influencer:
         return '인플루언서';
       default:
         return '알 수 없음';
+    }
+  }
+}
+
+enum BroadcastType { TV, Youtube }
+
+extension BroadcastTypeExtension on BroadcastType {
+  String description() {
+    switch (this) {
+      case BroadcastType.TV:
+        return 'TV';
+      case BroadcastType.Youtube:
+        return '유튜브';
     }
   }
 }
